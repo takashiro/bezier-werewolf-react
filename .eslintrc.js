64c8359bd0aeed75d9bf57ceb6512a535b6945de
Kazuichi Takashiro <takashiro@qq.com>
2022-11-03 22:07:41 +0800

    Setup React project

---
+++ .eslintrc.js
@@ -0,0 +1,71 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: [
+		'react',
+		'@typescript-eslint',
+	],
+	rules: {
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				ts: 'never',
+				tsx: 'never',
+				js: 'never',
+			},
+		],
+		'consistent-return': 'off',
+		indent: [
+			'error',
+			'tab',
+		],
+		'linebreak-style': 'off',
+		'max-len': 'off',
+		'no-await-in-loop': 'off',
+		'no-continue': 'off',
+		'no-plusplus': 'off',
+		'no-param-reassign': 'off',
+		'no-restricted-syntax': [
+			'error',
+			'WithStatement',
+		],
+		'no-shadow': 'off',
+		'no-tabs': 'off',
+		'no-use-before-define': 'off',
+		'react/jsx-indent-props': [
+			'error',
+			'tab',
+		],
+		'react/jsx-filename-extension': [
+			'error',
+			{ extensions: ['.tsx'] },
+		],
+		'react/jsx-indent': [
+			'error',
+			'tab',
+		],
+		'react/jsx-no-bind': 'off',
+		'react/jsx-props-no-spreading': 'off',
+		'react/require-default-props': 'off',
+		semi: 'off',
+	},
+	settings: {
+		'import/resolver': { typescript: {} },
+	},
+};
---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { transform } = require('@karuta/linguist');
+const { default: api } = require('@bezier/werewolf-server');
+
+/**
+ * @return {import('webpack').Configuration} Webpack configuration
+ */
+module.exports = function config(env, argv) {
+	const mode = argv && argv.mode === 'development' ? 'development' : 'production';
+	return {
+		mode,
+		entry: {
+			app: './src/index.tsx',
+		},
+		output: {
+			filename: 'static/[name].js',
+			path: path.resolve(__dirname, 'dist'),
+		},
+		resolveLoader: {
+			modules: [path.resolve(__dirname, 'node_modules')],
+		},
+		resolve: {
+			extensions: [
+				'.ts',
+				'.tsx',
+				'.js',
+			],
+		},
+		optimization: {
+			splitChunks: {
+				cacheGroups: {
+					vendors: {
+						test: /node_modules/,
+						name: 'vendor',
+						enforce: true,
+						chunks: 'all',
+					},
+				},
+			},
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'ts-loader',
+						options: {
+							getCustomTransformers() {
+								return {
+									before: [
+										transform(),
+									],
+								};
+							},
+						},
+					},
+				},
+				{
+					test: /\.scss$/,
+					exclude: /node_modules/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						{
+							loader: 'css-loader',
+							options: {
+								url: false,
+								sourceMap: mode === 'development',
+							},
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: mode === 'development',
+							},
+						},
+					],
+				},
+			],
+		},
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: 'static/[name].css',
+			}),
+			new HtmlWebpackPlugin({
+				template: 'src/index.html',
+			}),
+		],
+		devtool: mode === 'production' ? undefined : 'source-map',
+		devServer: {
+			static: path.join(__dirname, 'dist'),
+			compress: true,
+			port: 9526,
+			hot: true,
+			setupMiddlewares(middlewares, server) {
+				server.app.use('/api', api);
+				return middlewares;
+			},
+		},
+	};
+};
