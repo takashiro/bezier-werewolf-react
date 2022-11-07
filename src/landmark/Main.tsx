4d6e38d2fc9c45722a847f59534fc42a8efcde60
Kazuichi Takashiro <takashiro@qq.com>
2022-11-07 20:35:58 +0800

    Switch main content

--- src/landmark/Main.tsx
+++ src/landmark/Main.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import Context from '../page/Context';
 
 import Lobby from '../page/Lobby';
+
+function MainContent(): JSX.Element {
+	const [context] = React.useState(Context.Lobby);
+
+	switch (context) {
+	case Context.Lobby:
+	default:
+		return <Lobby />;
+	}
+}
 
 export default function Main(): JSX.Element {
 	return (
 		<main>
-			<Lobby />
+			<MainContent />
 		</main>
 	);
 }
---
+++ src/page/Context.ts
@@ -0,0 +1,5 @@
+const enum Context {
+	Lobby,
+}
+
+export default Context;
