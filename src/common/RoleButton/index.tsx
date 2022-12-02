fb76789215a94ad6c167f2e7f2134f32d5e90cd1
Kazuichi Takashiro <takashiro@qq.com>
2022-12-03 01:21:26 +0800

    Pass role into RoleButton

--- src/common/RoleButton/index.tsx
+++ src/common/RoleButton/index.tsx
@@ -1,38 +1,55 @@
 import React from 'react';
+import { useIntl } from 'react-intl';
 import classNames from 'classnames';
+
+import { Role } from '@bezier/werewolf-core';
 
 import Clickable from '../../base/Clickable';
 import RoleIcon from '../RoleIcon';
 
 import './index.scss';
 
+interface ChangeEvent {
+	role: Role;
+	selected: boolean;
+}
+
 interface RoleButtonProps {
-	icon: string;
-	name: string;
+	role: Role;
 	defaultSelected?: boolean;
+	onChange?(e: ChangeEvent): void;
 }
 
 export default function RoleButton(props: RoleButtonProps): JSX.Element {
+	const intl = useIntl();
 	const {
-		icon,
-		name,
+		role,
 		defaultSelected = false,
+		onChange,
 	} = props;
 
 	const [selected, setSelected] = React.useState(defaultSelected);
 
 	const toggle = React.useCallback(() => {
-		setSelected((prev) => !prev);
-	}, []);
+		setSelected((prev) => {
+			const cur = !prev;
+			onChange?.({ role, selected: cur });
+			return cur;
+		});
+	}, [setSelected, onChange]);
 
 	return (
 		<Clickable
 			className={classNames('role-button', { selected })}
 			onTrigger={toggle}
+			aria-pressed={selected}
 		>
 			<RoleIcon
-				icon={icon}
-				name={name}
+				icon={Role[role]}
+				name={intl.formatMessage({
+					id: `${Role[role].toLowerCase()}-name`,
+					defaultMessage: Role[role],
+				})}
 			/>
 		</Clickable>
 	);
---
+++ src/page/RoomCreator/RoleChangeEvent.ts
@@ -0,0 +1,8 @@
+import { Role } from '@bezier/werewolf-core';
+
+interface RoleChangeEvent {
+	role: Role;
+	num: number;
+}
+
+export default RoleChangeEvent;
