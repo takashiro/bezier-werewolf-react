65080bb97eeb30ea141c8577c7a9415736cdb3c7
Kazuichi Takashiro <takashiro@qq.com>
2023-04-16 16:42:12 +0800

    Test buttons on room creator

--- test/object/widget/Button.ts
+++ test/object/widget/Button.ts
@@ -1,8 +1,14 @@
+import { Locator } from '@playwright/test';
+
 import BasicLocator from '../common/BasicLocator';
 
 export default class Button extends BasicLocator {
 	async isPressed(): Promise<boolean> {
 		const pressed = await this.e.getAttribute('aria-pressed');
 		return pressed === 'true';
+	}
+
+	click(...args: Parameters<Locator['click']>): Promise<void> {
+		return this.e.click(...args);
 	}
 }
--- test/room-creator.spec.ts
+++ test/room-creator.spec.ts
@@ -1,9 +1,25 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-test('It opens home page', async ({ page }) => {
-	await page.goto('/#room-creator');
-	await page.screenshot({
-		path: 'output/room-creator.png',
-		fullPage: true,
+import RoomCreatorPage from './object/RoomCreatorPage';
+
+test('It operates on room creator', async ({ page }) => {
+	const creator = new RoomCreatorPage(page);
+
+	await test.step('open creator', async () => {
+		await creator.load();
+		await page.screenshot({
+			path: 'output/room-creator.png',
+			fullPage: true,
+		});
+	});
+
+	await test.step('check default options', async () => {
+		const main = creator.getMain();
+		const werewolf = main.getSpinButton({ name: 'Werewolf' });
+		expect(await werewolf.getValue()).toBe(2);
+		const minion = main.getButton({ name: 'Minion' });
+		expect(await minion.isPressed()).toBe(true);
+		await minion.click();
+		expect(await minion.isPressed()).toBe(false);
 	});
 });
