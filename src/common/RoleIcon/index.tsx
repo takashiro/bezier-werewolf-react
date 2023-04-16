f46292c245a972923098cdaceba24ddb5b8cdef8
Kazuichi Takashiro <takashiro@qq.com>
2023-04-16 15:52:22 +0800

    Make RoleIcon a figure role

--- src/common/RoleIcon/index.tsx
+++ src/common/RoleIcon/index.tsx
@@ -4,23 +4,26 @@ import classNames from 'classnames';
 import './index.scss';
 
 interface RoleIconProps {
+	id?: string;
 	icon: string;
 	name: string;
 }
 
 export default function RoleIcon(props: RoleIconProps): JSX.Element {
+	const captionId = React.useId();
 	const {
+		id,
 		icon,
 		name,
 	} = props;
 	const n = name.length;
 	return (
-		<div className="role-icon">
+		<div role="figure" id={id} className="role-icon" aria-labelledby={captionId}>
 			<div className="image">
 				<img className="background" src="style/card-bg.webp" alt="" />
-				<img className="content" src={`style/role/${icon}.webp`} alt={name} />
+				<img className="content" src={`style/role/${icon}.webp`} alt="" />
 			</div>
-			<div className={classNames('name', `n${n}`)}>
+			<div id={captionId} className={classNames('name', `n${n}`)}>
 				{name}
 			</div>
 		</div>
